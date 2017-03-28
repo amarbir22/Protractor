9d566c302cef5e51c6d962d544986bb3694c639f
Amarbir <amarbir22@gmail.com>
2017-03-28 16:18:57 -0400

    commit message

--- Test.js
+++ Test.js
@@ -1,7 +1,7 @@
 describe ('Test zoo site for bla bla', function(){
     it ('To text input field and check test output', function(){
         browser.get('https://docs.angularjs.org/tutorial');
-        element(by.model("q")).sendKeys("sdsdssssssssd");
+        element(by.model("q")).sendKeys("sdsdssssssdsdsssd");
 		 element(by.model("$ctrl.selectedVersion")).click();
 		 browser.sleep(4000);
     });

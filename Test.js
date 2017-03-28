9d35663261992a2d0d0d45887d688e251003db0d
xuvji06 <xuvji06@ca.com>
2017-03-28 16:06:14 -0400

    Add Angular website test

--- Test.js
+++ Test.js
@@ -1,7 +1,8 @@
 describe ('Test zoo site for bla bla', function(){
     it ('To text input field and check test output', function(){
-        browser.ignoreSynchronization = true;
-        browser.get('http://www.google.com');
-        element(by.model("")).sendKeys()
+        browser.get('https://docs.angularjs.org/tutorial');
+        element(by.model("q")).sendKeys("sdsdssssssssd");
+		 element(by.model("$ctrl.selectedVersion")).click();
+		 browser.sleep(4000);
     });
 });

22b232b0b651524b63ee6593791e8dd14b47af88
Amarbir <amarbir22@gmail.com>
2017-03-28 15:18:10 -0400

    Initial commit

---
+++ Test.js
@@ -0,0 +1,7 @@
+describe ('Test zoo site for bla bla', function(){
+    it ('To text input field and check test output', function(){
+        browser.ignoreSynchronization = true;
+        browser.get('http://www.google.com');
+        element(by.model("")).sendKeys()
+    });
+});

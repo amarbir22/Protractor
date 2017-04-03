4924f681f1b74ed1c51addcc0c84af5b4c692bd6
Amarbir <amarbir22@gmail.com>
2017-04-03 11:31:24 -0400

    Adding rest assured tests

--- ApiTesting/src/test/java/RestAssured/RestAssuredTest.java
+++ ApiTesting/src/test/java/RestAssured/RestAssuredTest.java
@@ -1,12 +1,17 @@
 package RestAssured;
 
 import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
 import com.jayway.restassured.response.Response;
 import com.jayway.restassured.response.ResponseBody;
-import org.junit.jupiter.api.Test;
+import com.jayway.restassured.response.ValidatableResponse;
+import org.junit.Test;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 
 /**
@@ -20,5 +25,16 @@ public class RestAssuredTest {
         Response res = RestAssured.when().get(uri);
         ResponseBody ff = res.body();
         System.out.println("body " + ff.prettyPrint());
+    }
+
+    @Test
+    public void testMessagenr() throws URISyntaxException {
+        URI uri = new URI("http://localhost:8080/messenger-app/webapi/messages/1");
+        ValidatableResponse hhh = given()
+                .accept(ContentType.JSON)
+                .when().get(uri)
+                .then()
+                .body("author", equalTo("John"));
+
     }
 }
